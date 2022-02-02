ea92b8c20dee0612e96d0547a687f2d7ffbfb7eb
Jo <yohanneszemed@gmail.com>
2022-02-02 18:39:07 +0300

    add javascript object

--- main.js
+++ main.js
@@ -12,6 +12,25 @@ const aboutLink = document.querySelector('#about');
 const about = document.querySelector('.about-me');
 const contactLink = document.querySelector('#contact');
 const contact = document.querySelector('.last-contact');
+const flow = document.querySelector('.overflow');
+
+const projectName = document.querySelector('.pop-desk-title');
+
+const projectDescription = document.querySelector('.msgs');
+const projectImage = document.querySelector('.pop-img');
+const projectTechnologies = document.querySelector('.puOut');
+const projectLink1 = document.querySelector('.live-link');
+const projectLink2 = document.querySelector('.git');
+// 1.  get all popup elements from the dom, which includes name, description, image, technologies //
+
+const projects = [{
+  name: 'Keeping track of hundreds of components website',
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+  image: 'icons/Snapshoot-Portfolio.png',
+  technologies: ['html', 'bootstrap', 'Ruby on rails'],
+  link1: 'https://yoz21.github.io/Portfolio/"',
+  link2: 'https://github.com/Yoz21/Portfolio',
+}];
 
 function closeBtn() {
   nav.classList.remove('desk-li-menu');
@@ -21,6 +40,7 @@ function closeBtn() {
 function cancelPop() {
   view.classList.remove('pop-up-menu');
   view.style.display = 'none';
+  flow.style.overflow = 'auto';
 }
 
 function openPortfolio() {
@@ -42,11 +62,27 @@ function navList() {
 function popList() {
   view.classList.add('pop-up-menu');
   view.style.display = 'block';
+  flow.style.overflow = 'hidden';
+
+  // for name, desciption, link, image, and technologies
+  // set their innerText with the first object from the array, i.e
+  // const project = projects[0]
+  // name.innerText =  project.name
+  const project = projects[0];
+
+  projectName.innerHTML = project.name;
+  projectDescription.innerHTML = project.description;
+  projectImage.setAttribute('src', project.image);
+  const projectTechnologiesList = project.technologies.reduce((acc, tehnology) => `${acc}<li>${tehnology}</li>`, '');
+  projectTechnologies.innerHTML = projectTechnologiesList;
+  projectLink1.setAttribute('href', project.link1);
+  projectLink2.innerHTML = project.link2;
 }
 
 list.forEach((element) => {
   element.addEventListener('click', closeBtn);
 });
+
 pop.addEventListener('click', popList);
 btn.addEventListener('click', navList);
 closePop.addEventListener('click', cancelPop);
